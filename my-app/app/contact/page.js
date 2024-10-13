0421f7ec8de9cc5738f8d1f987f1a62e088e1596
Shivani <shivanitripurari07@gmail.com>
2024-10-14 00:10:55 +0530

    update

--- my-app/app/contact/page.js
+++ my-app/app/contact/page.js
@@ -1,77 +1,77 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import './contact.css'
-import Footer from '../components/Footer'
-import Image from 'next/image'
-import call from '../assets/phone.svg'
-import location from '../assets/location.svg'
-const ContactPage = () => {
-  return (
-    <>
-      <div className='mt-20 mb-10 flex justify-center items-center'>
-        <div className='flex flex-col lg:flex-row xl:flex-row justify-center items-center h-[36rem]  w-[64rem]'>
-          <div className='h-full w-8/12 '>
-            <div className='bg-[#FABB88] h-full p-10 w-full '>
-            <div className=' flex flex-col lg:flex-row justify-start items-center pt-10'>
-              <form onSubmit="">
-                {/* <label for="name" className='text-[#1E3A45]'>Name</label> */}
-                <input id="name" placeholder='Name' className='p-3 mr-11 mb-7 text-base lg:text-lg text-[#000000] border-b-2 border-white w-40 lg:w-[36rem] focus:outline-none '/>
-                {/* <lable for="email">Email</lable> */}
-                <input id='email' placeholder="E-mail" className='p-3 mb-7 text-base lg:text-lg text-[#000000] border-b-2 border-white w-40 lg:w-[36rem] focus:outline-none '/>
-                <input id="name" placeholder='Country' className='p-3 mr-11 mb-7 text-base lg:text-lg text-[#000000] border-b-2 border-white w-40 lg:w-[36rem] focus:outline-none '/>
-                {/* <lable for="email">Email</lable> */}
-                <input id='email' placeholder="Subject" className='p-3 mb-7 text-base lg:text-lg text-[#000000] border-b-2 border-white w-40 lg:w-[36rem] focus:outline-none '/>
-                <textarea
-                    id='message'
-                    placeholder="Message"
-                    className='p-3 mb-7 text-base lg:text-lg text-[#000000] border-b-2 border-white w-40 lg:w-[36rem] focus:outline-none resize-none'
-                    rows="4"
-                />
-                {/**button ---> Request a call back */}
-              </form>
-            </div>
-          </div>
-        </div>
-          <div className='h-full w-4/12 '>
-            <div className='ml-2 p-1 flex flex-row justify-center items-center'>
-              <div className='h-40 w-40 md:h-32 md:w-32 lg:h-20 lg:w-20'><Image src={location} className='h-full w-full' /></div>
-              <div>
-                <p className='font-regular w-[100%] lg:w-[90%] text-xl md:text-base lg:text-lg p-3'>H.No 8-2-676/1/B/A/A/1&A,
-                   3rd Floor MVR Estate,
-                   Road No 13 Banjara Hills,
-                   Hyderabad 500034, India.
-                </p>
-              </div>
-            </div>
-            <div className=' p-1 flex flex-row justify-center items-center'>
-              <div className='h-7 w-7'><Image src={call} className='h-full w-full' /></div>
-              <div>
-                <p className=' font-regular text-xl w-[100%] md:text-base lg:text-lg p-3'>Landline: +91 40 4858 1777
-                   Mobile: +91 77990 89922
-                </p>
-              </div>
-            </div>
-            <div className='flex flex-col justify-start items-center'>
-              <h3 className='text-xl lg:text-2xl'>SALES INQUIRIES</h3>
-              <p className='ml-7 text-sm md:text-base lg:text-lg'>If you would have any enquiries related to sales,<br/>
-                 call us on ++91 7799089922</p>
-            </div>
-            <div className='flex flex-col justify-start items-center'>
-              <h3 className='text-xl lg:text-2xl'>CAREERS</h3>
-              <p className='ml-7 text-sm md:text-base lg:text-lg'>If you would like to work with us, send us your<br/> 
-                resume at info@ayatiproperties.com</p>
-            </div>
-            <div className='flex flex-col justify-start items-center'>
-              <h3 className='text-xl lg:text-2xl'>COLLABORATE</h3>
-              <p className='ml-7 text-sm md:text-base lg:text-lg'>If you would like us to take up a new project,<br/>
-                 send us a proposal at sales@ayatiproperties.com</p>
-            </div>
-          </div>
-        </div>
-        <div className='flex justify-center items-center'>{/* map */}</div>
-      </div>
-    </>
-  )
-}
+// import React from 'react'
+// import Navbar from '../components/Navbar'
+// import './contact.css'
+// import Footer from '../components/Footer'
+// import Image from 'next/image'
+// import call from '../assets/phone.svg'
+// import location from '../assets/location.svg'
+// const ContactPage = () => {
+//   return (
+//     <>
+//       <div className='mt-20 mb-10 flex justify-center items-center'>
+//         <div className='flex flex-col lg:flex-row xl:flex-row justify-center items-center h-[36rem]  w-[64rem]'>
+//           <div className='h-full w-8/12 '>
+//             <div className='bg-[#FABB88] h-full p-10 w-full '>
+//             <div className=' flex flex-col lg:flex-row justify-start items-center pt-10'>
+//               <form onSubmit="">
+//                 {/* <label for="name" className='text-[#1E3A45]'>Name</label> */}
+//                 <input id="name" placeholder='Name' className='p-3 mr-11 mb-7 text-base lg:text-lg text-[#000000] border-b-2 border-white w-40 lg:w-[36rem] focus:outline-none '/>
+//                 {/* <lable for="email">Email</lable> */}
+//                 <input id='email' placeholder="E-mail" className='p-3 mb-7 text-base lg:text-lg text-[#000000] border-b-2 border-white w-40 lg:w-[36rem] focus:outline-none '/>
+//                 <input id="name" placeholder='Country' className='p-3 mr-11 mb-7 text-base lg:text-lg text-[#000000] border-b-2 border-white w-40 lg:w-[36rem] focus:outline-none '/>
+//                 {/* <lable for="email">Email</lable> */}
+//                 <input id='email' placeholder="Subject" className='p-3 mb-7 text-base lg:text-lg text-[#000000] border-b-2 border-white w-40 lg:w-[36rem] focus:outline-none '/>
+//                 <textarea
+//                     id='message'
+//                     placeholder="Message"
+//                     className='p-3 mb-7 text-base lg:text-lg text-[#000000] border-b-2 border-white w-40 lg:w-[36rem] focus:outline-none resize-none'
+//                     rows="4"
+//                 />
+//                 {/**button ---> Request a call back */}
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//           <div className='h-full w-4/12 '>
+//             <div className='ml-2 p-1 flex flex-row justify-center items-center'>
+//               <div className='h-40 w-40 md:h-32 md:w-32 lg:h-20 lg:w-20'><Image src={location} className='h-full w-full' /></div>
+//               <div>
+//                 <p className='font-regular w-[100%] lg:w-[90%] text-xl md:text-base lg:text-lg p-3'>H.No 8-2-676/1/B/A/A/1&A,
+//                    3rd Floor MVR Estate,
+//                    Road No 13 Banjara Hills,
+//                    Hyderabad 500034, India.
+//                 </p>
+//               </div>
+//             </div>
+//             <div className=' p-1 flex flex-row justify-center items-center'>
+//               <div className='h-7 w-7'><Image src={call} className='h-full w-full' /></div>
+//               <div>
+//                 <p className=' font-regular text-xl w-[100%] md:text-base lg:text-lg p-3'>Landline: +91 40 4858 1777
+//                    Mobile: +91 77990 89922
+//                 </p>
+//               </div>
+//             </div>
+//             <div className='flex flex-col justify-start items-center'>
+//               <h3 className='text-xl lg:text-2xl'>SALES INQUIRIES</h3>
+//               <p className='ml-7 text-sm md:text-base lg:text-lg'>If you would have any enquiries related to sales,<br/>
+//                  call us on ++91 7799089922</p>
+//             </div>
+//             <div className='flex flex-col justify-start items-center'>
+//               <h3 className='text-xl lg:text-2xl'>CAREERS</h3>
+//               <p className='ml-7 text-sm md:text-base lg:text-lg'>If you would like to work with us, send us your<br/> 
+//                 resume at info@ayatiproperties.com</p>
+//             </div>
+//             <div className='flex flex-col justify-start items-center'>
+//               <h3 className='text-xl lg:text-2xl'>COLLABORATE</h3>
+//               <p className='ml-7 text-sm md:text-base lg:text-lg'>If you would like us to take up a new project,<br/>
+//                  send us a proposal at sales@ayatiproperties.com</p>
+//             </div>
+//           </div>
+//         </div>
+//         <div className='flex justify-center items-center'>{/* map */}</div>
+//       </div>
+//     </>
+//   )
+// }
 
-export default ContactPage
+// export default ContactPage
